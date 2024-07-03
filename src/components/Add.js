48b7542f85adcb06dd20a8682b306f44ca6d4d2e
Hemanth Pulakandam <75231529+hemanthgithub642@users.noreply.github.com>
2024-07-03 23:45:17 +0530

    Add files via upload

---
+++ src/components/Add.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { ResultCard } from "./ResultCard";
+
+export const Add = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=094d048308c16281a7e6b4f941b77028&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.results) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setResults([]);
+      });
+  };
+
+  return (
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange}
+            />
+          </div>
+
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((movie) => (
+                <li key={movie.id}>
+                  <ResultCard movie={movie} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
